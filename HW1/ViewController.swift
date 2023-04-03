0639b3a3af37321fbdc7a65ee93319a98d6cf62a
Nurik <N-TNT@MAIL.RU>
2023-04-03 15:53:51 +0600

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Нурлан on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
